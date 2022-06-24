2951ce5b16ffa990f2e4890108f631f613fba72b
abhishek123mojumdar <abhishek123mojumdar@gmail.com>
2022-06-24 19:43:28 +0530

    1 file modified

--- index.js
+++ index.js
@@ -28,36 +28,64 @@ Function.prototype.myBind = function (...args) {
 
 abhiObj.foo.myBind(myObj, 1, 2)();
 
-//----------------- change a recursive object to a flat object --------------// 
+//------------map polly-fill --------------//
+
+Array.prototype.myMap = function (cb) {
+  let temp = [];
+  for (let i = 0; i < this.length; i++) {
+    temp.push(cb(this[i], i, this));
+  }
+  return temp;
+};
+
+var arr = [4, 54, 6, 6].myMap((x) => {
+  return x * 2;
+});
+
+console.log(arr);
+
+//----------------- change a recursive object to a flat object --------------//
 
 var abhiObj = {
-  name:'Abhishek',
-  address : {
-    personal : {
-      city : 'Jamshedpur',
-      area : 'Dimna Road'
+  name: 'Abhishek',
+  address: {
+    personal: {
+      city: 'Jamshedpur',
+      area: 'Dimna Road',
     },
-    official : {
-      city : 'Bangalore',
-      area : { 
-        landMark : 'smart point'
-      }
-    }
-  }
-}
+    official: {
+      city: 'Bangalore',
+      area: {
+        landMark: 'smart point',
+      },
+    },
+  },
+};
 
-
-function flatObject (abhiObj,obj,prevKeyName){
+function flatObject(abhiObj, obj, prevKeyName) {
   for (let key in abhiObj) {
-    if ( abhiObj.hasOwnProperty(key) && (typeof abhiObj[key]) == 'object') {
-      flatObject(abhiObj[key] , obj , prevKeyName +'_'+ key)
+    if (abhiObj.hasOwnProperty(key) && typeof abhiObj[key] == 'object') {
+      flatObject(abhiObj[key], obj, prevKeyName + '_' + key);
     } else {
-      obj[prevKeyName+'_'+key] = abhiObj[key]
+      obj[prevKeyName + '_' + key] = abhiObj[key];
     }
   }
-  
-  return obj
+
+  return obj;
 }
 
-let obj=  {}
-console.log(flatObject(abhiObj,obj,'person'))
+let obj = {};
+console.log(flatObject(abhiObj, obj, 'person'));
+
+//---------------- add n number ------------------//
+let sum = function (a) {
+  return function (b) {
+    if (b) {
+      return sum(a + b);
+    } else {
+      return a;
+    }
+  };
+};
+
+console.log(sum(1)(2)(3)(4)(5)(6)(7)());
